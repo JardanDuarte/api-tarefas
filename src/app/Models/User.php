e44135df53f53c01c17be923d1d15aff7f5258a2
JardanDuarte <jardansilva2012@gmail.com>
2026-04-05 19:33:16 -0300

    feat(model): adiciona models e relacionamento com usuário
    - cria model Task para representar as tarefas
    - cria model Comment para representar os comentários
    - adiciona método tasks no model User para relacionamento com Task via Eloquent ORM

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -15,7 +17,12 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public function tasks() : HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 
     /**
      * Get the attributes that should be cast.
